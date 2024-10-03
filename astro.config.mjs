33f5f744e4eea8c92924a2f135572187143ffed4
Junxuan Bao <Junxuan.Bao@Gmail.com>
2024-10-02 22:23:06 -0400

    multi language support

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,18 +6,73 @@ import starlight from '@astrojs/starlight';
 export default defineConfig({
 	integrations: [
 		starlight({
-			title: 'My Docs',
+			title: {
+				en: 'Junxuan Bao',
+				'zh-CN': '鲍俊轩',
+			  },
 			social: {
 				github: 'https://github.com/withastro/starlight',
 			},
 			editLink: {
 				baseUrl: 'https://github.com/JunxuanB/Junxuan-Bao-Personal-Website/edit/main/',
 			},
+			// Set English as the default language for this site.
+			defaultLocale: 'zh-cn',
+			locales: {
+			  // English docs in `src/content/docs/en/`
+			  en: {
+				label: 'English',
+			  },
+			  // Simplified Chinese docs in `src/content/docs/zh-cn/`
+			  'zh-cn': {
+				label: '简体中文',
+				lang: 'zh-CN',
+			  },
+			},
 			sidebar: [
+				{
+					label: "Full Stack Tutorial",
+					translations: {
+						'en': "Full Stack Tutorial",
+						'zh-CN': "全栈教程",
+					},
+					badge: {
+						text: {
+							'en': "Incomplete",
+							'zh-CN': "未完成",
+						},
+						variant: 'tip'
+					  },
+					items: [
+						{
+							label: "Frontend",
+							translations: {
+								'en': "Frontend",
+								'zh-CN': "前端",
+							},
+							autogenerate: { directory: 'full-stack-tutorial/frontend' },
+						},
+						{
+							label: "Backend",
+							translations: {
+								'en': "Backend",
+								'zh-CN': "后端",
+							},
+							autogenerate: { directory: 'full-stack-tutorial/backend' },
+						},
+						{
+							label: "Project: E-Fridge",
+							translations: {
+								'en': "Project: E-Fridge",
+								'zh-CN': "项目：赛博冰箱",
+							},
+							autogenerate: { directory: 'full-stack-tutorial/project-e-fridge' },
+						},
+					]
+				},
 				{
 					label: 'Guides',
 					items: [
-						// Each item here is one entry in the navigation menu.
 						{ label: 'Example Guide', slug: 'guides/example' },
 					],
 				},
@@ -28,4 +83,7 @@ export default defineConfig({
 			],
 		}),
 	],
+	redirects: {
+		'/': '/en',
+	},
 });
