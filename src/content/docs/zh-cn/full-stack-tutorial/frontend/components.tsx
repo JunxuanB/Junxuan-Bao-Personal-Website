fc9a10816c04d89ef1fe9ca494767681bf40a0fd
Junxuan Bao <Junxuan.Bao@Gmail.com>
2024-11-23 22:10:17 -0500

    component

---
+++ src/content/docs/zh-cn/full-stack-tutorial/frontend/components.tsx
@@ -0,0 +1,42 @@
+interface NoticeProps {
+    title: string;
+    content: string;
+}
+
+export const Notice = ( props: NoticeProps ) => {
+  return (
+    <div
+      style={{
+        width: '100%',
+        border: '1px dotted #aaa',
+        borderRadius: '5px',
+        padding: '10px',
+        position: 'relative',
+        backgroundColor: '#f9f9f9',
+        boxShadow: '0 2px 5px rgba(0, 0, 0, 0.1)',
+      }}
+    >
+      <h4
+        style={{
+          margin: 0,
+          paddingBottom: '5px',
+          fontSize: '16px',
+          color: '#333',
+          borderBottom: '1px solid #ddd',
+        }}
+      >
+        {props.title}
+      </h4>
+      <p
+        style={{
+          margin: '10px 0 0',
+          fontSize: '14px',
+          color: '#555',
+        }}
+      >
+        {props.content}
+      </p>
+    </div>
+  );
+};
+
